9fef703250b58883f2bb0e67cda6f7925680d628
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-15 14:41:16 +0900

    add chips

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {MDCTextField} from '@material/textfield';
+import {MDCChipSet} from '@material/chips/chip-set';
 
 import './index.scss';
 
 document.querySelectorAll('.mdc-text-field').forEach(e => {
     new MDCTextField(e);
 });
+
+new MDCChipSet(document.querySelector('.mdc-evolution-chip-set'));
