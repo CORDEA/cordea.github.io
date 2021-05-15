6d2fb31ca86cd3fb7be2849e0580fca3b587b54a
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-15 18:09:17 +0900

    allow chips to be added

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ document.querySelectorAll('.mdc-text-field').forEach(e => {
     new MDCTextField(e);
 });
 
+new MDCList(document.querySelector('.mdc-list'));
+new MDCRipple(document.querySelector('.mdc-button'));
 new MDCChipSet(document.querySelector('.mdc-evolution-chip-set'));
 
-new MDCList(document.querySelector('.mdc-list'));
-
-new MDCRipple(document.querySelector('.mdc-button'));
+const skillChipSet = document.querySelector('#skill-chip-set--span');
+const skillTextField = document.querySelector('#skill-text-field--input');
+skillTextField.addEventListener('keydown', function (e) {
+    if (e.key !== 'Enter' && e.keyCode !== 13) {
+        return;
+    }
+    const value = skillTextField.value;
+    if (value.length <= 0) {
+        return;
+    }
+    const template = document.querySelector('#skill-chip');
+    const chip = template.content.cloneNode(true);
+    chip.querySelector('.mdc-evolution-chip__text-label').textContent = value;
+    skillChipSet.appendChild(chip);
+});
